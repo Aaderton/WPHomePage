567ac4b2fccc0bbb6b50928126a483abc405af79
Aatu Pekkanen <31401068+Aaderton@users.noreply.github.com>
2017-11-24 15:58:32 +0200

    Add files via upload

---
+++ my-theme/page-home.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * Template Name: Koti testi
+ */
+ ?>
+<?php get_header(); ?>
+<!-- Intro Header -->
+<header class="intro">
+  <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>"
+  width="<?php echo get_custom_header()->width; ?>" alt="Banner Image" />
+    <div class="intro-body">
+        <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="brand-heading">Welcome</h1>
+                        <p class="intro-text">I'm Aatu, a Web Designer from Finland.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+   <div id=content>
+    <!-- About Section -->
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <?php
+                // the query
+                $about_posts = new WP_Query( array(
+                        'category_name' => 'about'
+                      )
+                    );
+                    if ( $about_posts->have_posts() ) : ?>
+                    <!-- the loop -->
+                    <?php while ( $about_posts->have_posts() ) : $about_posts->the_post(); ?>
+                        <div class="row">
+                        <?php the_post_thumbnail('medium') ?>
+                        </div>
+                        <h2><?php the_title(); ?></h2>
+                        <p><?php the_content(); ?></p>
+                        <P>Testi</p>
+                    <?php endwhile; ?>
+                    <!-- end of the loop -->
+                    <?php wp_reset_postdata(); ?>
+                <?php else : ?>
+                    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+                <?php endif; ?>
+            </div>
+        </div>
+    </section>
+
+    <!-- Portfolio Section -->
+    <section id="portfolio" class="content-section text-center">
+        <div class="portfolio-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>My Portfolio</h2>
+                    <p>Here you can take a closer look at some of my previous works.</p>
+                </div>
+
+                <div class="row form-group">
+        <div class="col-xs-12 col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">MLB Games</h3>
+                </div>
+                <div class="panel-image hide-panel-body">
+                    <img src="koti/img/mlb.png" class="panel-image-preview" alt="Real estate site prototybe"/>
+                </div>
+                <div class="panel-body">
+                    <h4>MLB Games finder</h4>
+                    <p>This is a project I did for a programming course during my exchange in Canada.
+                      It uses AJAX and JSON to
+                      list MLB Games by the date the user inputs. It also highlights Toronto Blue Jays Games.</p>
+                    <a target="_blank" href="http://student.labranet.jamk.fi/~H3183/mlb/">Link to the page</a>
+                </div>
+                <div class="panel-footer text-center">
+                    Click the image for more info
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Mapping Application</h3>
+                </div>
+                <div class="panel-image hide-panel-body">
+                    <img src="koti/img/map.jpg" class="panel-image-preview" alt="Java snake game"/>
+                </div>
+                <div class="panel-body">
+                    <h4>Bar finder map application</h4>
+                    <p>Another project I did during my exchange in Canada.
+                      This application uses Bing maps, JSON and Factual data to find the user's location
+                       20 bars/restaurants in an area around the user.
+                      It also displays info on the bars by using JavaScript objects with the Factual JSON data.</p>
+                </div>
+                <div class="panel-footer text-center">
+                    Click the image for more info
+                </div>
+            </div>
+        </div>
+    </div>
+
+                <div class="row form-group">
+        <div class="col-xs-12 col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Website prototype</h3>
+                </div>
+                <div class="panel-image hide-panel-body">
+                    <img src="koti/img/portfolio1.jpg" class="panel-image-preview" alt="Real estate site prototybe"/>
+                </div>
+                <div class="panel-body">
+                    <h4>Prototype of a front page</h4>
+                    <p>This is a website prototype I did for a web design course using Bootstrap, HTML5 and CSS3.</p>
+                    <a target="_blank" href="http://student.labranet.jamk.fi/~H3183/visualisointi/harj2/">Link to the page</a>
+                </div>
+                <div class="panel-footer text-center">
+                    Click the image for more info
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Java Snake</h3>
+                </div>
+                <div class="panel-image hide-panel-body">
+                    <img src="koti/img/portfolio2.png" class="panel-image-preview" alt="Java snake game"/>
+                </div>
+                <div class="panel-body">
+                    <h4>Java Snake game</h4>
+                    <p>Me and my friend programmed a classic Snake type game with a High Score list using Java.
+                      My responsibilites were half of the coding, the snake's movement and most of the graphics.</p>
+                </div>
+                <div class="panel-footer text-center">
+                    Click the image for more info
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row form-group">
+        <div class="col-xs-12 col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Raving Space</h3>
+                </div>
+                <div class="panel-image hide-panel-body">
+                    <img src="koti/img/portfolio3.PNG" class="panel-image-preview" alt="Space game" class="panel-image-preview" />
+                </div>
+                <div class="panel-body">
+                    <h4>Web based Space Shooter on Phaser framework</h4>
+                    <p>I was a part of a group of three students for a web project where we made a web based spaceship shooter
+                      using JavaScript and Phaser framework, HTML5, CSS3, PHP and MySQL.</p>
+                    <p>I handled the game's sound menu, item shop, part of the graphics and enemy AI.</p>
+                </div>
+                <div class="panel-footer text-center">
+                    Click the image for more info
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Questionnaire Game</h3>
+                </div>
+                <div class="panel-image hide-panel-body">
+                    <img src="koti/img/portfolio4.jpg" class="panel-image-preview" alt="Questionnaire"/>
+                </div>
+                <div class="panel-body">
+                    <h4>Web based questionnaire for children</h4>
+                    <p>This was my first group project with a few other students. We designed and made a children's
+                      game that works on a browser.</p>
+                    <p>I designed and programmed the game's questionnaire using HTML5, CSS3, JavaScript, JQuery and JSON.</p>
+                </div>
+                <div class="panel-footer text-center">
+                    Click the image for more info
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Contact Section -->
+<section id="contact" class="container content-section text-center">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2">
+            <h2>Contact info</h2>
+            <p>aatu.pekkanen@gmail.com</p>
+            <ul class="list-inline banner-social-buttons">
+                <li>
+                    <a target="_blank" href="https://www.facebook.com/aatu.pekkanen.3" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+<?php get_footer();?>
